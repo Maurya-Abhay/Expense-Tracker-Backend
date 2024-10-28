ec3340006841731b87689af127bd0b192b30434e
Maurya-Abhay <167536919+Maurya-Abhay@users.noreply.github.com>
2024-10-28 14:31:48 +0530

    AuthController.js

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -45,8 +45,7 @@ const login = async (req, res) => {
         }
         const jwtToken = jwt.sign(
             { email: user.email, _id: user._id },
-            process.env.JWT_SECRET,
-            { expiresIn: '2h' }
+            process.env.JWT_SECRET
         )
 
         res.status(200)
