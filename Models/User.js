6e0bec402c91e114c3b92945a922030557a1d04c
Maurya-Abhay <167536919+Maurya-Abhay@users.noreply.github.com>
2024-11-28 22:21:27 +0530

    User.js

--- Models/User.js
+++ Models/User.js
@@ -1,4 +1,6 @@
+
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -33,5 +35,17 @@ const UserSchema = new Schema({
     ]
 });
 
-const UserModel = mongoose.model('users', UserSchema);
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const UserModel = mongoose.model('User', UserSchema);
 module.exports = UserModel;
